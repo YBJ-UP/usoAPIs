41acbc3d51a02d8c6bdedaf45b8b87d83093b360
ya <243678@ids.upchiapas.edu.mx>
2025-09-29 22:26:44 -0600

    tenia un error xd

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import * as el from "./scripts/html_elements.js"
 import { loadPagePhotos } from "./components/vistaFotos.js";
 import { setGifs } from "./components/vistaGifs.js";
 
-loadPage()
+loadPagePhotos()
 
 el.navPhotoButton.addEventListener('click', loadPagePhotos)
 
